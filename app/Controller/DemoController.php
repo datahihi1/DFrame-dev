d59215967373067c8ec9ab4abd608dea4429b5d7
Dat Duy <datahihi1100@gmail.com>
2025-11-04 23:05:18 +0700

    Test captcha

--- app/Controller/DemoController.php
+++ app/Controller/DemoController.php
@@ -1,12 +1,40 @@
 <?php
+
 namespace App\Controller;
 
 use Core\Application\Router;
+use Core\Application\View;
+use Gregwar\Captcha\CaptchaBuilder;
 
-class DemoController extends Controller{
+class DemoController extends Controller
+{
 
-    #[Router(path:'/haha', method:'GET', isApi:false, name:'demo.haha', middleware: null)]
-    public function demo(){
+    #[Router(path: '/haha', method: 'GET', isApi: false, name: 'demo.haha', middleware: null)]
+    public function demo()
+    {
         return "oke";
+    }
+    #[Router(path: '/captcha', method: 'GET', name: 'show.captcha')]
+    public function captcha()
+    {
+        $builder = new CaptchaBuilder();
+        $builder->build();
+
+        // Lưu mã CAPTCHA vào session
+        $_SESSION['captcha'] = $builder->getPhrase();
+
+        return View::render('captcha', ['builder' => $builder]);
+    }
+    #[Router(path: '/verify-captcha', method: 'POST', name: 'verify.captcha')]
+    public function verifyCaptcha()
+    {
+        $captcha = $_POST['captcha'] ?? '';
+        if ($captcha === $_SESSION['captcha']) {
+            // CAPTCHA đúng
+            return "CAPTCHA đúng";
+        } else {
+            // CAPTCHA sai
+            return "CAPTCHA sai";
+        }
     }
 }
